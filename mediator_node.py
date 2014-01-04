72095039568a7494b72ab4e867449dff8bc096e5
Nick W <falconeer001@gmail.com>
2014-01-04 21:47:12 +0000

    Better handling of redundancy

--- mediator_node.py
+++ mediator_node.py
@@ -232,7 +232,6 @@ class FincryptMediatorFactory(protocol.ServerFactory):
 				self.files[filename]['snodes']['list'].append(snodes[y][0])
 				count += 1
 			y += 1
-		print self.files[filename]
 	
 	def parse_message(self, line):
 		data = pickle.loads(base64.b64decode(line))
